18e504b38ae44b59461f17c3cdc93fc93186649c
Antoine BARBIER <antbarbi@f4r11s9.clusters.42paris.fr>
2025-09-10 12:59:45 +0200

    new features for smart contract

--- code/token.sol
+++ code/token.sol
@@ -6,11 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Simple NFT contract with minting capabilities
+// Simple NFT contract with minting, burning, and base URI capabilities
 contract SimpleNFT is ERC721, ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
+    string private _baseTokenURI;  // Base URI for metadata, e.g., "https://ipfs.io/ipfs/your-hash/"
 
-    constructor() ERC721("42Nug", "42n") Ownable(msg.sender) {}
+    constructor() ERC721("42NugArt", "42NA") Ownable(msg.sender) {}
 
     // Function to mint new NFTs, restricted to the owner
     function mintNFT(address to, string memory tokenURI) public onlyOwner {
@@ -18,6 +19,22 @@ contract SimpleNFT is ERC721, ERC721URIStorage, Ownable {
         _tokenIdCounter++;
         _mint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
+    }
+
+    // Function to burn NFTs, allowed for owner or token holder
+    function burn(uint256 tokenId) public {
+        require(ownerOf(tokenId) == msg.sender || msg.sender == owner(), "Not authorized to burn");
+        _burn(tokenId);
+    }
+
+    // Function to set base URI for metadata, restricted to owner
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    // Override _baseURI to return the set base URI
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
     }
 
     // Override to support tokenURI
